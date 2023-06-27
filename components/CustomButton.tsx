bbeeefea55f9bf40b9eeb0f45e012856d30bae75
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-06-27 12:23:00 -0600

    Feat: Add two searchbar views

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,7 +2,7 @@
 import { CustomButtonProps } from "@/types"
 import Image from "next/image"
 
-const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, handleClick, btnType, textStyles, rightIcon, isDisabled}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
@@ -10,9 +10,19 @@ const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButt
 			className={`custom-btn ${containerStyles}`}
 			onClick={handleClick}
     >
-			<span className={`flex-1`}>
+			<span className={`flex-1 ${textStyles}`}>
 				{title}
 			</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image 
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain" 
+          />
+        </div>
+      )}
     </button>
   )
 }
