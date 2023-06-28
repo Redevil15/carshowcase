fa1fdd36e18b1f330db86dc3a477544b818ede01
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-06-27 19:52:33 -0600

    Feat: Add searchBar append newPath functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { useState } from "react"
+import React, { useState } from "react"
 
 import SearchManufacturer from "./SearchManufacturer"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 const SearchButton = ({otherClasses}: { otherClasses: string}) => (
   <button
@@ -23,8 +24,36 @@ const SearchButton = ({otherClasses}: { otherClasses: string}) => (
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('')
   const [model, setModel] = useState('')
+  const router = useRouter()
 
-  const handleSearch = () => {}
+  const handleSearch = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault()
+
+    if(manufacturer === '' && model === ' '){
+      return alert('please fill in the search bar')
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
+  }
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search)
+    if(model){
+      searchParams.set('model', model)
+    } else {
+      searchParams.delete('model')
+    }
+
+    if(manufacturer){
+      searchParams.set('manufacturer', manufacturer)
+    } else {
+      searchParams.delete('manufacturer')
+    }
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+    router.push(newPathname)
+  }
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
